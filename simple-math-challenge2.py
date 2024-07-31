88d2819528287ddbe7da697ca2aa7e27e3d1dac4
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-31 05:44:18 -0400

    Create simple-math-challenge2.py

---
+++ simple-math-challenge2.py
@@ -0,0 +1,21 @@
+# Student name
+# Current date
+# Simple Math Challenge #2
+
+# Part 1 / Perimeter Calculation
+
+# Write a script that:
+# lets the user enter two integers (numbers without a decimal point)
+# The first number represents the WIDTH of a rectangle (in feet)
+# The second number represents the HEIGHT of the rectangle (in feet)
+# Calculate and display the perimeter of the rectangle (in feet)
+
+
+
+
+# Part 2 / Tip Calculator
+# Write a script that:
+# lets the user enter the price of lunch at a local restaurant
+# calculates the tip for the server (at 15 percent)  (HINT: Convert 15% to its decimal equivalent)
+# displays the tip and the total price of the lunch separately
+# What built-in Python function lets you treat the user's input as a floating-point number? (A number containing a decimal point)
