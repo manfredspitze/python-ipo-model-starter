9bcfb76d819503f7f542237bdf0ea47417c1bc6d
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-31 05:31:28 -0400

    Update primm-do-now4.py

--- primm-do-now4.py
+++ primm-do-now4.py
@@ -1,3 +1,22 @@
 # Student name
 # Current date
 # Predict / Math with Input
+
+# Task 1
+# Add comments to predict what the code below will do
+# Run the script in your code editor to see if your prediction was correct
+num1 = input("Enter a number:\n")
+num2 = 10
+
+
+result = num1 * num2
+
+
+print(result)
+
+
+
+# Task 2
+# Copy and update the script from Task 1 so that
+# Python treats the user's input as an integer
+# What built-in Python function can you use to convert input to an integer?
