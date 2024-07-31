cafd52da3df08107b279febcd3796c87880987da
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-31 05:35:49 -0400

    Create simple-math-challenge.py

---
+++ simple-math-challenge.py
@@ -0,0 +1,11 @@
+# Student name
+# Current date
+# Simple Math Challenge
+
+# Write a script that:
+
+# lets the user enter two integers (numbers without a decimal point)
+# The first number represents the WIDTH of a rectangle (in feet)
+# The second number represents the HEIGHT of the rectangle (in feet)
+# Calculate and display the area of the rectangle (in square feet)
+# Include the units of measure as part of your output
