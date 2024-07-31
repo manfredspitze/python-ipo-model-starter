8ee7116f6ef731bdabe890151b1568032ecd55b8
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-31 05:24:19 -0400

    Update primm-do-now3.py

--- primm-do-now3.py
+++ primm-do-now3.py
@@ -1,1 +1,37 @@
+# Student name
+# Current date
+# Predict / Changing Variables
+
+# Task1 
+# Add comments to the code to predict the output on lines 16, 17 and 21
+
+
+name1 = "Carlos"
+name2 = "Michael"
+name3 = "Hunter"
+name4 = "Rachel"
+name5 = "Alyssa"
+
+
+print(name1 + " and " + name4)
+print(name3)
+
+
+name3 = "Christina"
+print(name3)
+
+# Task 2 
+# Create a variable that stores the number 20
+# Output the value stored in the variable
+# Multiply the variable by 10
+# Output the updated value
+
+
+
+
+
+
+
+# Task 3
+# Write a script that uses variables and outputs a countdown from 10 to 0
 
