5e7f89833a56b2ba593a16b63e9c4ef2b221b48d
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-07-31 05:17:01 -0400

    Update primm-do-now2.py

--- primm-do-now2.py
+++ primm-do-now2.py
@@ -1,1 +1,20 @@
+# Student name
+# Current date
+# Predict / Variables & Operators
 
+# Task 1 
+# Add comments to predict what the code below will do
+
+
+num1 = 20
+num2 = 5
+
+
+result = num1 * num2
+
+
+print(result)
+
+
+# Task 2 
+# Write code that uses numbers stored in two variables and performs a calculation to output the number 42
