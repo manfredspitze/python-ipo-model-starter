b58af0c1ef6bab1037a879e146d98537d5fb9a0d
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-23 09:50:55 -0400

    Update primm-do-now4.py

--- primm-do-now4.py
+++ primm-do-now4.py
@@ -5,6 +5,7 @@
 # Task 1
 # Add comments to predict what the code below will do
 # Run the script in your code editor to see if your prediction was correct
+# HINT: What do you have to remember about how the input ( ) function works?
 num1 = input("Enter a number:\n")
 num2 = 10
 
