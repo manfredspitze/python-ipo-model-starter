52d1599377fa88c952682578cd80f80c197ee392
Manfred Spitze <155758012+manfredspitze@users.noreply.github.com>
2024-08-23 09:47:05 -0400

    Update primm-do-now3.py

--- primm-do-now3.py
+++ primm-do-now3.py
@@ -35,4 +35,5 @@ print(name3)
 # Task 3
 # Write a script that uses variables and outputs a countdown from 10 to 0
 # HINT: See *Making Numerical Lists* in *Python Crash Course*, p. 57
+# HINT: You'll have to use a NEGATIVE step value to get the loop to count backwards
 
